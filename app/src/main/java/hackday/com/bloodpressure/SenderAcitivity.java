2fd8cfd6f234b6105abf31592087d6126ddc1fc8
Daniyar Mukhanov <mukhanov.daniyar@gmail.com>
2016-01-31 12:07:47 +0000

    before submit

--- app/src/main/java/hackday/com/bloodpressure/SenderAcitivity.java
+++ app/src/main/java/hackday/com/bloodpressure/SenderAcitivity.java
@@ -2,10 +2,12 @@ package hackday.com.bloodpressure;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -23,9 +25,17 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class SenderAcitivity extends AppCompatActivity {
 
@@ -167,6 +177,80 @@ public class SenderAcitivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+    private class CreateOrder extends AsyncTask {
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+        ProgressDialog progressDialog;
+        String email, password,api_password,is_user,text,type,price,name,city,address,phone;
+
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+            progressDialog = new ProgressDialog(SenderAcitivity.this);
+            progressDialog.setMessage("Please wait...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setCancelable(true);
+
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            Boolean authorized=sharedPreferences.getBoolean("authorized",false);
+            email=sharedPreferences.getString("email","");
+            password=authorized?sharedPreferences.getString("password",""):"";
+            api_password="artlinesAa!";
+            is_user=authorized?"yes":"no";
+            text=sharedPreferences.getString("text","");
+            type=sharedPreferences.getString("type","");
+            price=sharedPreferences.getString("price","");
+            name=authorized?"":sharedPreferences.getString("name","");
+            city=authorized?"":sharedPreferences.getString("city","");
+            address=authorized?"":sharedPreferences.getString("address","");
+            phone=authorized?"":sharedPreferences.getString("phone","");
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("email", email));
+            params.add(new BasicNameValuePair("password", password));
+            params.add(new BasicNameValuePair("api_password", api_password));
+            params.add(new BasicNameValuePair("is_user", is_user));
+            params.add(new BasicNameValuePair("text", text));
+            params.add(new BasicNameValuePair("type", type));
+            params.add(new BasicNameValuePair("price", price));
+            params.add(new BasicNameValuePair("name", name));
+            params.add(new BasicNameValuePair("city", city));
+            params.add(new BasicNameValuePair("address", address));
+            params.add(new BasicNameValuePair("phone", phone));
+            Log.e("FOR CREATE ORDER",email+"\n"+password+"\n"+api_password+"\n"+is_user+"\n"+text+"\n"+type+"\n"+price+"\n"+name+"\n"+city+"\n"+address+"\n"+phone);
+
+            jsonObject = jsonParser.makeHttpRequest(""+"order/APICreate", "POST", params);
+            Log.d("Order", jsonObject.toString());
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            progressDialog.dismiss();
+            try {
+                if(jsonObject.getString("result").equals("success")) {
+                    //startActivity(new Intent(Payment.this, Success.class));
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "Network problem, try again", Toast.LENGTH_LONG).show();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 }
